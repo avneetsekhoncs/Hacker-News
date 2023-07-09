0b913db0303cf05cb39d590d11fa7a1dfd69d83a
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-09 13:42:14 -0700

    Main page layout setup

--- Hacker News/ContentView.swift
+++ Hacker News/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }.navigationTitle("HACKER NEWS")
         }
-        .padding()
     }
 }
 
@@ -24,3 +22,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello 1"),
+    Post(id: "2", title: "Hello 2"),
+    Post(id: "3", title: "Hello 3")
+]
