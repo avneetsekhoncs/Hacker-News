662eeb9e33ac94f97c2856d346d085726414e23f
Avneet Sekhon <avneetsekhon88@gmail.com>
2023-07-09 13:23:50 -0700

    Base project

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Avneet Sekhon on 2023-07-09.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
